fc85157ca05be9002e8eaf7ed160050ce66fe4fc
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-10 14:58:24 +0300

    Add files via upload

---
+++ button.ornek.24/button.ornek.24/button.ornek.24/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int sayac = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSayac.Text = "0";
+        }
+
+        private void btnAzalt_Click(object sender, EventArgs e)
+        {
+            if (sayac>-10)
+            {
+                sayac--;
+            }
+            lblSayac.Text=sayac.ToString();
+        }
+
+        private void btnArttir_Click(object sender, EventArgs e)
+        {
+            if (sayac<10)
+            {
+                sayac++;
+            }
+            lblSayac.Text = sayac.ToString();
+
+        }
+    }
+}
